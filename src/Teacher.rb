ddcd3b609039fd2bf5214dcf9823117fe34069a7
ahadb802 <abdulahadb802@gmail.com>
2023-03-08 21:20:22 +0500

    remove linters error

--- src/Teacher.rb
+++ src/Teacher.rb
@@ -1,11 +1,10 @@
 class Teacher < Person
-    def initialize(age, specialization, name = "Ahad", parent_permission = true)
-      super(age, name, parent_permission)
-      @specialization = specialization
-    end
-    
-    def can_use_services?
-      true
-    end
+  def initialize(age, specialization, name = 'Ahad', parent_permission: true)
+    super(age, name, parent_permission)
+    @specialization = specialization
   end
-  
+
+  def can_use_services?
+    true
+  end
+end
