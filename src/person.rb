98afb2207b8c87fea356c7cf9a9e29410731ed90
ahadb802 <abdulahadb802@gmail.com>
2023-03-08 23:20:33 +0500

    update person file for many to many rel

--- src/person.rb
+++ src/person.rb
@@ -1,15 +1,21 @@
 require_relative './nameable'
 
 class Person < Nameable
-  attr_accessor :id, :name, :age
+  # ...
 
-  def initialize(age, name = 'Unknown', parent_permission: true)
+  attr_reader :rentals
+
+  def initialize(_age, _name = 'Unknown', *)
     super()
-    @id = rand(1..1000)
-    @name = name
-    @age = age
-    @parent_permission = parent_permission
+    # ...
+    @rentals = []
   end
+
+  def rent(book, date)
+    Rental.new(date, self, book)
+  end
+
+  # ...
 
   def can_use_services?
     is_of_age? || @parent_permission
